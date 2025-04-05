8f2b2c99caf62c173db426dc3924f0d148d0665a
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-05 09:09:07 +0200

    Create MutatedProcess.h

---
+++ RunnableISH/Kernel/Processes/MutatedProcess.h
@@ -0,0 +1,6 @@
+//if we take courses of mpsi (french prepa) and forbid heat transfer
+//with maxwell equations, we can use extra datas of processes.
+//especizlly, we can do a cross product of extra data vectors to have
+//a more logical behaviour of processes. this is self-resilient and 
+//it will help us forbidding zomby viruses. but this technology
+//must be enhanced at user dpace.
