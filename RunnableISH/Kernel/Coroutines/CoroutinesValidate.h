711fe4dcb0587a38e294b852b1a3cc917db35766
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-05 07:20:34 +0200

    Create CoroutinesValidate.h

---
+++ RunnableISH/Kernel/Coroutines/CoroutinesValidate.h
@@ -0,0 +1,3 @@
+//we may be able to get signatures if we dont use an algorithm.
+//and we can parse hard disk 23* integral .... hard disk datas (heat transfer).
+//a modified value but not resilient to components cyber attacks.
