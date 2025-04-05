3fb075c52bcaeaba70d78fce64d0dbad7d3d7ad4
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-05 10:22:09 +0200

    Create Schedulled.h

---
+++ RunnableISH/Kernel/Schedulled/Schedulled.h
@@ -0,0 +1,5 @@
+//we have to take 4 gfoups of 2 processes to
+//have a non heat transfer logics ! then, it is possible to
+//use linux scheduller internally. but for a server, it will bebbetter
+//to use onlynlinux sxheduller. after that, 4grouos of 2 orocesses
+//seems more decure.
