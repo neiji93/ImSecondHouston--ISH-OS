957219d415aa9df804ee4334da7215ac33ad1dce
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-08 11:46:17 +0200

    Create ApproximateMetaData.h

---
+++ RunnableISH/Kernel/Schedulled/ApproximateMetaData.h
@@ -0,0 +1,4 @@
+//we can use an heuristic or ai to prevent
+//kernel meta data recovery bybusers.
+//this feature doenst need any jardware specifications.
+//but it would be better ro have some requirements in the futur.
