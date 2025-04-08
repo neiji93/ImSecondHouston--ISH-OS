ed4c291d077587bc08e71c9b3af3200dbc18cca0
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-08 13:40:35 +0200

    Create CoroutinesToCommandsHzndlers.h

---
+++ RunnableISH/Kernel/CoroutinesToCommandsHzndlers.h
@@ -0,0 +1,4 @@
+//as we dont use zny specific cpu requirements,
+//we dont need any low level langage.
+//plus, heurstics can be transformed.
+//then is suited here. lets use layering.of common python clzsses.
