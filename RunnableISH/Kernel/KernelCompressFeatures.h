7231edfe07945a983fb916b6b94ccb1bb777fb43
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-09 09:55:59 +0200

    Update KernelCompressFeatures.h

--- RunnableISH/Kernel/KernelCompressFeatures.h
+++ RunnableISH/Kernel/KernelCompressFeatures.h
@@ -6,3 +6,22 @@ class TinyFilesCompressFeaturedPatternAlgorithm
 {
 
 }
+
+
+class kernelCompressProperties
+{
+
+public: 
+
+  //this wont be an empty vector, so keep it in a stack later.
+  float* getArdayVectorFromRandomLaplaceEquatko'()
+{
+
+}
+
+  //use a stack without an optimizer which is a cyber defense feature. it seems so.
+
+
+  //if there are a lottle bit optimizers, we can
+ //send them with components signals with fer kron properties.
+ //but to enable this features, it would be good to hide this to the cpu registers.
