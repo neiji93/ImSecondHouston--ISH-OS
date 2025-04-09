e34262822b172c26350d1c25cd5b6eccc05312ab
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-09 09:49:09 +0200

    Create KernelCompressFeatures.h

---
+++ RunnableISH/Kernel/KernelCompressFeatures.h
@@ -0,0 +1,8 @@
+//as we already used heuristics sometimes,
+//can use laplace forces and ondelettes to compress the kernel.
+//the result can be a vector with its own properties.
+
+class TinyFilesCompressFeaturedPatternAlgorithm
+{
+
+}
