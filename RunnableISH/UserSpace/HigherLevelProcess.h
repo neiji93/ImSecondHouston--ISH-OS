0d36159e6bf19ed90a694b39d83cb787bf8a1466
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-14 08:52:37 +0200

    Create HigherLevelProcess.h

---
+++ RunnableISH/UserSpace/HigherLevelProcess.h
@@ -0,0 +1,6 @@
+//if we take the cipher 2-2-4-7, 
+/rhere is no meaning in heat transfer.
+//then, processes can be shared with cryptographic signals
+//but not with real planck signals 
+//on the other hand we can use risk pattern (hooking functions or metadatas of controllers 
+  //or defines cpp in userspace programs).
