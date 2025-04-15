040e8c75f573f9915eee5b3984bc9853a0697bc7
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-15 16:09:17 +0200

    Create HeuristicUnknownProxessCancelor.h

---
+++ RunnableISH/UserSpace/HeuristicUnknownProxessCancelor.h
@@ -0,0 +1,4 @@
+//dont permit heuristics in unknown owner process
+//recognizer because it will iberload cpu ticks.
+//but we can it in kernel mode because of coupled systems.
+//as there is no meaning in physics, the kernel mode is hided.
