f0f5c5d69dad836e37e8f30ee1e414c54962afdd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-16 13:01:46 +0200

    Update DefenderO e.h

--- RunnableISH/Kernel/NetworkDefense/DefenderO e.h
+++ RunnableISH/Kernel/NetworkDefense/DefenderO e.h
@@ -2,4 +2,8 @@
 //we can use cpu ticks foe the kernel as we previously 
 //swapped cpu overloads. in the cpu ticks, use linix inodes.
 //there lust be 
-approximatively 250 ...
+approximatively 250 processes of blanck datas
+//running at 3,1 ns. the timer here is used for futur
+//purpose without aiming userspace necsuse in 
+//hest transfer related docs, 0,0097 value can be found in phsyics
+//courses tests !
