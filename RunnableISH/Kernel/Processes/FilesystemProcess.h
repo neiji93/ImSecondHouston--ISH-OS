20445d55e8807e000aa7a0446a2440175b936a68
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-16 11:27:25 +0200

    Create FilesystemProcess.h

---
+++ RunnableISH/Kernel/Processes/FilesystemProcess.h
@@ -0,0 +1,7 @@
+//as we have two ciohers at the moelent (2 2 4 7 10 and 2 2 4 7 10 3),
+//we will have to use both esuations and models
+(smart pointers) to handle missi'g files owners. then, the futur cpu or gpu
+  //tick wont have to check if attacks comes from ceyptographic or mean field games technologies 
+  //in order tonhave better approximated equations, we will...
+
+  
