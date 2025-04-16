72cfe1978de107fbcc4bcca34fbcdf6ebf3089ce
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-16 11:43:35 +0200

    Create DefenderO e.h

---
+++ RunnableISH/Kernel/NetworkDefense/DefenderO e.h
@@ -0,0 +1,5 @@
+//if files are sent to the kernel,
+//we can use cpu ticks foe the kernel as we previously 
+//swapped cpu overloads. in the cpu ticks, use linix inodes.
+//there lust be 
+approximatively 250 ...
