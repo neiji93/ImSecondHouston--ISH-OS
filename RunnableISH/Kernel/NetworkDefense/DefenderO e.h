0f843d837bd70b274e6ad626364a3007ec5215a8
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-17 12:43:43 +0200

    Update DefenderO e.h

--- RunnableISH/Kernel/NetworkDefense/DefenderO e.h
+++ RunnableISH/Kernel/NetworkDefense/DefenderO e.h
@@ -7,3 +7,11 @@ approximatively 250 processes of blanck datas
 //purpose without aiming userspace necsuse in 
 //hest transfer related docs, 0,0097 value can be found in phsyics
 //courses tests !
+
+
+//it seems we can use incremental henon maos
+//see: https://www.google.com/search?client=ms-android-xiaomi-terr1-rso2&sca_esv=1c493ad901dc2a31&sxsrf=AHTn8zr3ZyYdtMxEO-d0L15lhKL0gotXAg%3A1744886451757&q=incremental%20henon%20map&udm=36&source=lnms&fbs=ABzOT_BnMAgCWdhr5zilP5f1cnRvJ3SHQcDVxkdpDyHwlRhdNaDfwjhN1BUZy5Lq6wtEN8BJtKKkLdMtCw3vJdTF28KIGADs29RdO7jcj_C6wD201SAUCX5nTn9ex9DwAu4XHHk3wXVIeCJVdZ9l4bvYI_KyYufsSKezIIbITrqEDaoFSehNilM92PNSOJoGW6dJq9sXZswe&sa=X&ved=2ahUKEwiBoIXJ8N6MAxUHTaQEHe3JEw8Q0pQJegUIjAEQAQ
+//so we can omit coroutines here and async keyword.
+//then we can set ...
+//i tried to ise navier stokes esuations for os previously 
+
