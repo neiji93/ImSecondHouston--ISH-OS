bce03a365f9c025303c94862d453b4de9f0e8e43
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-18 09:20:09 +0200

    Update MixedRepositoriesPackages.h

--- RunnableISH/VFS/MixedRepositoriesPackages.h
+++ RunnableISH/VFS/MixedRepositoriesPackages.h
@@ -2,3 +2,18 @@
 //and models. then, the tail of it will be to
 //have utility classes that can swap files.
 //this is also due to the correctness of the files memory adresses.
+
+//to get correctness we will have to mount
+//the files degixes with no coroutine support.
+//this is the second time we dont use asy'c or coroutines.
+//then, hdre, the coroutines (not async) can be used
+//to mode an adress.
+class  //this would be good if a cyber attack is kot bzsed on meqn field game
+{
+
+}
+
+class HDFindAfressMechanicsAlgorithmsCeypted
+{
+
+}
