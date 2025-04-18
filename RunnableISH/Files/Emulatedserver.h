e3f04f1fc9d9e14d8c7ac65d255f519e6eaa158b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-18 22:19:16 +0200

    Create Emulatedserver.h

---
+++ RunnableISH/Files/Emulatedserver.h
@@ -0,0 +1,8 @@
+//a server cannot directly access files but
+//we can use new ciphers to complete unused heat transfer equation if we have an uniq pid.
+//so here the emulator is an old model of pointer. this server can generate pids.
+
+class FirstHashGenerator //can fail
+{
+
+}
