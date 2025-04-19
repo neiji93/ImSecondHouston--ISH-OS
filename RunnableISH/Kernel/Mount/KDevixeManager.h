735eb9999cad83446709629b0812e94a1238debd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-19 11:49:01 +0200

    Create KDevixeManager.h

---
+++ RunnableISH/Kernel/Mount/KDevixeManager.h
@@ -0,0 +1,8 @@
+//we have three models znd we have to take two variables od each models 
+//Then, we have to have three starting vortual devices.
+//So, this is an alternative of serializing. so, lets send an error message to the new devixes 
+
+class kDeviceErrorMessageSenderwithoutASM
+{
+
+}
