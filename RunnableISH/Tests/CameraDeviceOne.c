2f93929a41d8af99f4180770defbebd3eccbc0d1
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-20 11:14:48 +0200

    Update CameraDeviceOne.c

--- RunnableISH/Tests/CameraDeviceOne.c
+++ RunnableISH/Tests/CameraDeviceOne.c
@@ -1,5 +1,14 @@
 
+void startenergy()
+{
+//we have to get a model that is suited for heat transfer equations.
+  //so lets test the model twice.
+  einsum(gotmodel);
+  einsum(gormodel);
+  //we have to cancel ciphers now. lets donit rwoxe acain.
 
+  //now lets see what cpu register it can use
+}
 
 void createCaleraDevixd()
 {
